af9adf6be591c834927b52393c3d631eaa2fcfdf
Muhammad-Rabi693 <rktiger693@gmail.com>
2024-12-04 18:52:11 +0500

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+    import React from 'react'
+    const App = () => {
+        return (
+            <View className=" bg-black flex-1 items-center justify-center">
+            <Text className='text-2xl font-bold text-white'>Hello || Welcome To Expo Nativewind Template</Text>
+            </View>
+        )
+    }
+    export default App
